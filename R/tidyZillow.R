69719eb5f681222b6d107b05c477fe32bd05d3bc
Chandler Lutz <chandler.lutz@gmail.com>
2017-08-07 10:17:53 -0400

    updated package to use data.table

--- R/tidyZillow.R
+++ R/tidyZillow.R
@@ -7,7 +7,7 @@
 ##to tidy zillow data
 
 
-#' Tidy zillow time series research data
+#' Tidy zillow time series research data using data.table
 #'
 #' see \url{http://www.zillow.com/research/data/}
 #'
@@ -16,36 +16,38 @@
 #' data
 #' @param series.name a string with the series name. default is
 #' "value"
-#' @return a tidy dataframe with the zillow data
+#' @return a tidy data.table with the zillow data
 #' @author Chandler Lutz
 #' @examples
 #' ##state level data
-#' library(readr)
-#' zillow.state <- read_csv("http://files.zillowstatic.com/research/public/State/State_Zhvi_AllHomes.csv")
+#' library(data.table)
+#' url <- "http://files.zillowstatic.com/research/public/State/State_Zhvi_AllHomes.csv"
+#' zillow.state <- fread(url)
 #' zillow.state <- tidyZillow(zillow.state, series.name = "ZillowState")
 #' head(zillow.state)
-#' @importFrom dplyr %>%
 #' @export
 tidyZillow <- function(zillow.df, series.name = "value") {
-
 
     ##The column names
     names.zillow <- names(zillow.df)
     ##The column names which match the times
     names.times <- names.zillow[grepl("[0-9]{4}-[0-9]{2}", names.zillow)]
+    ##The id columns
+    names.id <- names.zillow %>% .[!(. %in% names.times)]
 
-    #clean the data
+    ##Initialize the time column to pass R CMD check with data.table
+    time <- NULL
+
     zillow.out <- zillow.df %>%
-        tidyr::gather_(key_col = "time", value_col = series.name,
-                       gather_cols = names.times)
-    zillow.out$time <- as.Date(paste0(zillow.out$time, "-01"))
-
-    ##update the names by and the "Zillow" prefix
-    names.zillow <- names(zillow.out)
-    names.zillow[1:(ncol(zillow.out) - 2)] <- paste0(
-        "Zillow",
-        names.zillow[1:(ncol(zillow.out) - 2)])
-    names(zillow.out) <- names.zillow
+        ##set as data.table
+        setDT %>%
+        ##melt
+        melt(id.vars = c(names.id),
+             variable.name = "time",
+             variable.factor = FALSE,
+             value.name = c(series.name)) %>%
+        ##Create the time column
+        .[, time := as.Date(paste0(time, "-01"))]
 
     return(zillow.out)
 
--- R/tidyZillow_main.R
+++ R/tidyZillow_main.R
@@ -5,6 +5,12 @@
 #'
 #' The only function you're likely to need from \pkg{tidyZillow} is
 #' \code{\link{tidyZillow}}.
+#' @importFrom magrittr %>%
+#' @import data.table
 #' @aliases NULL
 "_PACKAGE"
+
+##For magrittr dot. From https://github.com/tidyverse/magrittr/issues/29
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
+
 
